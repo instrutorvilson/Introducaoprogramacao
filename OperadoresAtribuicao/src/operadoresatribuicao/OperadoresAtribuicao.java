07d5686c1126711643ba9f4e9033e54f1e73c2c8
vilsonmoro <vilsonmoro@gmail.com>
2021-05-25 19:32:53 -0300

    Operadores de atribuição

---
+++ OperadoresAtribuicao/src/operadoresatribuicao/OperadoresAtribuicao.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadoresatribuicao;
+
+/**
+ *
+ * @author vilso
+ */
+public class OperadoresAtribuicao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int x;
+       x = 5;
+       System.out.println(x);
+       //x += 3;
+       x = x + 3;
+       System.out.println(x);
+      // x -=2;
+       x*=2;
+       System.out.println(x);
+    }
+    
+}
