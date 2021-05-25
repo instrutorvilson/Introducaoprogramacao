0fe2d6925259791f672d0d18ad0d41f918b04995
vilsonmoro <vilsonmoro@gmail.com>
2021-05-25 19:24:38 -0300

    Operadores Matematicos

---
+++ OperadoresMatematicos/src/operadoresmatematicos/OperadoresMatematicos.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadoresmatematicos;
+
+/**
+ *
+ * @author vilso
+ */
+public class OperadoresMatematicos {
+
+   
+    public static void main(String[] args) {
+       /* + - * / %  ++ -- */
+     /*  int x = 25;
+       int y = 10;
+       
+       System.out.println(x + y);
+       System.out.println(x - y);
+       System.out.println(x * y);
+       System.out.println(x / y);
+       System.out.println(x % y); //modulo -> resto da divisão
+      */
+       int z = 1;
+       System.out.println(z);
+       //z++;
+       z = z + 1;
+       System.out.println(z);
+       z--;
+       System.out.println(z);
+    }
+    
+}
