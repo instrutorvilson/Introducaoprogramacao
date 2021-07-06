d6b3af037e6f68880a1ff60c8ad6780c25d05629
vilsonmoro <vilsonmoro@gmail.com>
2021-07-06 20:44:59 -0300

    Criar método mostrarDados

--- ConceitosPOO/src/conceitospoo/ConceitosPOO.java
+++ ConceitosPOO/src/conceitospoo/ConceitosPOO.java
@@ -12,16 +12,15 @@ public class ConceitosPOO {
        Pessoa paulo = new Pessoa();
        paulo.nome = "Paulo da Silva";
        paulo.idade = 50;
+       paulo.altura = 1.80f;
+       paulo.peso = 90;
+       paulo.mostrarDados();
        
-       System.out.println(paulo.nome);
-       System.out.println(paulo.idade);
        
        Pessoa p2 = new Pessoa();
        p2.nome = "Januario da Silva";
-       p2.idade = 80;
-       
-       System.out.println(p2.nome);
-       System.out.println(p2.idade);
+       p2.idade = 80;       
+       p2.mostrarDados();
     }
     
 }
--- ConceitosPOO/src/conceitospoo/Pessoa.java
+++ ConceitosPOO/src/conceitospoo/Pessoa.java
@@ -3,9 +3,28 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+/*
+  1) - Criar um metodo para verificar 
+    se a pessoa é menor de idade;
+
+  2) criar método para verificar se pessoa 
+   está no pessoa ideal IMC.
+*/
 package conceitospoo;
 
 public class Pessoa {
-  String nome;
-  int idade;  
+
+    String nome;
+    int idade;
+    float peso;
+    float altura;
+
+    public void mostrarDados() {
+        System.out.println("Nome...: " + this.nome);
+        System.out.println("Idade..: " + this.idade);
+        System.out.println("Altura.: " + this.altura);
+        System.out.println("Peso...: " + this.peso);
+        System.out.println("");
+    }
 }
