f2bd7666da1b2bd9108faa581167a06b2a140d44
vilsonmoro <vilsonmoro@gmail.com>
2021-07-13 20:33:00 -0300

    Criando listas com objetos

--- ColecaoList/src/colecaolist/ColecaoList.java
+++ ColecaoList/src/colecaolist/ColecaoList.java
@@ -15,7 +15,25 @@ import java.util.List;
 public class ColecaoList {
 
     public static void main(String[] args) {
-        String nome;
+        List<Pessoa> pessoas = new ArrayList<>();
+        
+        Pessoa p = new Pessoa();
+        p.nome = "Jose";
+        p.idade = 30;
+        p.peso = 50;
+        p.altura = 1.50f;
+        pessoas.add(p);
+        
+        pessoas.add(new Pessoa("Ana", 15));
+        pessoas.add(new Pessoa("Pedro",55,100,1.89f));
+        
+        //percorrer
+        for(Pessoa pessoa : pessoas){
+            System.out.println(pessoa.toString());  
+        }
+                
+        
+       /* String nome;
         String[] nomes = {"joao", "Jose"};
 
        // ArrayList<String> cars = new ArrayList<String>();
@@ -32,7 +50,7 @@ public class ColecaoList {
         System.out.println(cars);
         System.out.println(cars.contains("Jeep"));
         System.out.println(cars.indexOf("Jeep"));
-       
+       */
     }
 
 }
